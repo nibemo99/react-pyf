6e4b5f8d433bce235a8ec75b487dc9149b81efd7
Nicolas <nibemo99@gmail.com>
2022-09-11 02:11:24 -0500

    Historial completely working

--- src/Components/Main/Historial/Historial.js
+++ src/Components/Main/Historial/Historial.js
@@ -8,20 +8,41 @@ export const Historial = ({ setMainDisplay }) => {
   //variables
 
 
-
   // funciones
   function clickHandler() {
     setMainDisplay('menu')
   }
+  function eliminarRegistro(event) {
+    let textoBoton = event.target.innerText
+    if (textoBoton === 'Eliminar') {
+      event.target.innerText = '¿Definitivo?'
+    }
+    if (textoBoton === '¿Definitivo?') {
+      parsedHistory.splice(renderLista - 1, 1)
+      localStorage.historial = JSON.stringify(parsedHistory)
+      event.target.innerText = 'Eliminado!'
+      setTimeout(() => {
+        setRenderLista(0)
+      }, 1500);
+    }
+  }
+
+
+  // Con cada render
+  var parsedHistory = localStorage.historial
+  if (!parsedHistory) {
+    localStorage.historial = JSON.stringify([])
+  }
+  parsedHistory = JSON.parse(localStorage.historial)
 
 
   return (
 
-    <div className=' w-4/5 mx-auto mb-10 flex flex-col h-[450px] overflow-y-auto'>
+    <div className=' w-4/5 mx-auto mb-10 flex flex-col h-[450px] '>
 
       <button
         onClick={clickHandler}
-        className='shadow-sm shadow-blue-700 py-4 mx-auto my-2 w-1/2 hover:bg-blue-700 hover:text-white hover:scale-105 ease-out duration-500 focus:bg-blue-700 focus:text-white focus:scale-105'
+        className='shadow-sm shadow-blue-700 py-4 mx-auto my-4 w-1/2 hover:bg-blue-700 hover:text-white hover:scale-105 ease-out duration-500 focus:bg-blue-700 focus:text-white focus:scale-105'
 
       >Regresar</button>
 
@@ -32,18 +53,15 @@ export const Historial = ({ setMainDisplay }) => {
 
           {
             // Traigo el localstorage y lo mapeo para renderizar cada uno de las partidas.
-            JSON.parse(localStorage.historial).map((element, index) => {
+            parsedHistory.map((element, index) => {
               return (
                 <button
                   key={index}
                   onClick={() => { setRenderLista(index + 1) }}
                   className=' py-1 mx-5 my-2 hover:scale-105 ease-out duration-500 grid grid-cols-2 items-center gap-4 shadow-sm shadow-blue-700 hover:shadow-md  hover:shadow-blue-700 focus:shadow-blue-700 focus:shadow-md focus:scale-105'
-
                 >
 
-                  <p
-                    className='text-2xl tracking-widest hover:scale-110 ease-in-out duration-500 w-fit mx-auto'
-
+                  <p className='text-2xl tracking-widest hover:scale-110 ease-in-out duration-500 w-fit mx-auto'
                   >
                     {element.data[element.data.length - 1].numero}
                   </p>
@@ -67,9 +85,45 @@ export const Historial = ({ setMainDisplay }) => {
       {/* SI RENDERLISTA !== 0, renderiza el item con index === RENDERLISTA */}
       {(renderLista !== 0) && (
         <div>
-          <h1>Esta es la partida # : {renderLista}</h1>
-          <h3>Fecha {JSON.parse(localStorage.historial)[renderLista].date}</h3>
-          <button onClick={(e) => { setRenderLista(0); console.log(renderLista); }} >Volver</button>
+
+          <div className='grid grid-cols-2'>
+            <h1>{parsedHistory[renderLista - 1].date}</h1>
+            <h1>Rondas: {parsedHistory[renderLista - 1].data.length}</h1>
+          </div>
+
+          <div className='my-4 max-h-[275px] overflow-auto grid grid-cols-3 mx-auto w-3/5'>
+            {/* Espacio para renderizar las rondas: */}
+
+            <p className='bg-blue-200 text-lg'>Picas</p>
+            <p className='bg-blue-200 text-lg'>Número</p>
+            <p className='bg-blue-200 text-lg'>Fijas</p>
+
+            {
+
+
+              parsedHistory[renderLista - 1].data.map(e => {
+                return (
+                  <>
+                    <p className='py-[2px]' key={`${e.picas}p`}>{e.picas}</p>
+                    <p className='py-[2px]' key={`${e.numero}n`}>{e.numero}</p>
+                    <p className='py-[2px]' key={`${e.fijas}f`}>{e.fijas}</p>
+                  </>
+                )
+              })
+            }
+
+
+          </div>
+
+          <button
+            onClick={(e) => { setRenderLista(0) }}
+            className='shadow-sm shadow-blue-700 w-1/4 mx-2 hover:scale-105 ease-out duration-500 hover:shadow-md  hover:shadow-blue-700 focus:shadow-blue-700 focus:shadow-md focus:scale-105'
+          >Volver</button>
+          <button
+            onClick={eliminarRegistro}
+            className='shadow-sm shadow-red-700 w-1/4 mx-2 hover:scale-105 ease-out duration-500 hover:shadow-md  hover:shadow-red-700 focus:shadow-blue-700 focus:shadow-md focus:scale-105 '
+          >Eliminar</button>
+
         </div>
       )}
 
