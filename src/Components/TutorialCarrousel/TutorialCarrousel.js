7c6df632a2a42e4b9599b90970d284471365d874
Nicolas <nibemo99@gmail.com>
2022-09-11 20:48:22 -0500

    Created carrousel component

---
+++ src/Components/TutorialCarrousel/TutorialCarrousel.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+export const TutorialCarrousel = () => {
+    // USESTATES
+    const [tutPage, setTutPage] = useState(0)
+
+    // Variables
+    let titulos = [
+        '',
+        '',
+        '',
+    ]
+
+    return (
+        <>
+            {(tutPage === 0) && (
+                <>
+        
+                </>
+            )}
+        </>
+    )
+}
