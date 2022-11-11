37189b46845dbf62bbe0585dad23bb271b9603c7
nibemo99 <49243236+nibemo99@users.noreply.github.com>
2022-11-11 04:21:38 -0500

    Added claps and buzzer sounds

--- src/utils/audios.js
+++ src/utils/audios.js
@@ -1,7 +1,11 @@
 import successAudio from '../audio/success.mp3'
 import whoshAudio from '../audio/whosh.mp3'
 import wowAudio from '../audio/wow.mp3'
+import buzzerAudio from '../audio/buzzer.mp3'
+import clapsAudio from '../audio/claps.mp3'
 
 export const success = new Audio( successAudio )
 export const whosh = new Audio( whoshAudio )
 export const wow = new Audio( wowAudio )
+export const buzzer = new Audio( buzzerAudio )
+export const claps = new Audio( clapsAudio )
